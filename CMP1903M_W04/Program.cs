81ed0102571eb2fef34687f800c9a3a98ea83f44
Mark Doughty <mdoughty@lincoln.ac.uk>
2020-02-07 17:15:44 +0000

    List created

---
+++ CMP1903M_W04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CMP1903M_W04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //a list of names
+            List<string> names = new List<string>();
+
+            //Add names to the list here:
+
+
+            //Loop through the collection, printing the names here:
+        }
+    }
+}
